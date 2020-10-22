007ba158e652030cdee85d75fb1c3b71ad3a436f
eugeizquierdo <meizquierdo@uc.cl>
2020-10-22 10:14:54 -0300

    5

--- Entrega2/ocupacion_fechas.sql
+++ Entrega2/ocupacion_fechas.sql
@@ -4,7 +4,7 @@ RETURNS TABLE (iid int, capacidad int, atraque timestamp) AS $$
 BEGIN
 RETURN QUERY EXECUTE 'SELECT muelles.iid, instalaciones.capacidad, permisos.atraque
 		FROM muelles, para_m, permisos, instalaciones
-		WHERE t1 = $1 and t2= $2 and instalaciones.iid = muelles.iid and muelles.iid = para_m.iid and para_m.per_id = permisos.per_id and permisos.atraque >= $1 and permisos.atraque <= $2'
+		WHERE fecha_1 = $1 and fecha_2 = $2 and instalaciones.iid = muelles.iid and muelles.iid = para_m.iid and para_m.per_id = permisos.per_id and permisos.atraque >= fecha_1 and permisos.atraque <= fecha_2'
 	USING t1,t2;
 	RETURN;
 END
