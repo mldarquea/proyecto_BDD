0dc406e59ed0735d42eb6eba315ea056ee198811
mldarquea <mldarquea@uc.cl>
2020-10-22 09:59:10 -0300

    2

--- Entrega2/ocupacion_fechas.sql
+++ Entrega2/ocupacion_fechas.sql
@@ -2,9 +2,9 @@ CREATE OR REPLACE FUNCTION
 ocupacion_fechas (t1 date, t2 date)
 RETURNS TABLE (iid int, capacidad int, atraque timestamp) AS $$
 BEGIN
-RETURN QUERY EXECUTE 'SELECT muelles.iid, muelles.capacidad, permisos.atraque
-		FROM muelles, para_m, permisos
-		WHERE muelles.iid = para_m.iid and para_m.per_id = permisos.per_id and permisos.atraque >= t1 and permisos.atraque <= t2'
+RETURN QUERY EXECUTE 'SELECT muelles.iid, instalaciones.capacidad, permisos.atraque
+		FROM muelles, para_m, permisos, instalaciones
+		WHERE instalaciones.iid = muelles.iid and muelles.iid = para_m.iid and para_m.per_id = permisos.per_id and permisos.atraque >= t1 and permisos.atraque <= t2'
 	USING t1,t2;
 	RETURN;
 END
