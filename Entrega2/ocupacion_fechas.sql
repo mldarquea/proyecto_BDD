8d85e67d7184e8b23ce33ddee6db96f41365e225
mldarquea <mldarquea@uc.cl>
2020-10-22 09:53:17 -0300

    sin

--- Entrega2/ocupacion_fechas.sql
+++ Entrega2/ocupacion_fechas.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION
 ocupacion_fechas (t1 date, t2 date)
-RETURNS TABLE (muelles.iid int, muelles.capacidad int, permisos.atraque) AS $$
+RETURNS TABLE (iid int, capacidad int, atraque) AS $$
 BEGIN
 RETURN QUERY EXECUTE 'SELECT muelles.iid, muelles.capacidad, permisos.atraque
 		FROM muelles, para_m, permisos
