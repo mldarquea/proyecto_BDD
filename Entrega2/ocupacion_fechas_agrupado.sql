3a64d2f1f11a581fccdee21f8f05a507da972476
eugeizquierdo <meizquierdo@uc.cl>
2020-10-22 10:45:10 -0300

    agrup4

--- Entrega2/ocupacion_fechas_agrupado.sql
+++ Entrega2/ocupacion_fechas_agrupado.sql
@@ -4,7 +4,7 @@ RETURNS TABLE (iid int, capacidad int, atraque timestamp) AS $$
 BEGIN
 RETURN QUERY EXECUTE 'SELECT muelles.iid, instalaciones.capacidad, permisos.atraque
 		FROM muelles, para_m, permisos, instalaciones
-		WHERE instalaciones.iid = muelles.iid and muelles.iid = para_m.iid and para_m.per_id = permisos.per_id and permisos.atraque >= $1 and permisos.atraque <= $2' GROUP BY instalaciones.iid'
+		WHERE instalaciones.iid = muelles.iid and muelles.iid = para_m.iid and para_m.per_id = permisos.per_id and permisos.atraque >= $1 and permisos.atraque <= $2 GROUP BY instalaciones.iid'
 	USING t1,t2;
 	RETURN;
 END
