491e60d1fd3be87ca78d2ac9bc88c0f9bbaf0d8e
eugeizquierdo <meizquierdo@uc.cl>
2020-10-22 10:50:21 -0300

    sin capacidad

--- Entrega2/ocupacion_fechas_agrupado.sql
+++ Entrega2/ocupacion_fechas_agrupado.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION
 ocupacion_agrupado (t1 date, t2 date)
 RETURNS TABLE (iid int, capacidad int, atraque timestamp) AS $$
 BEGIN
-RETURN QUERY EXECUTE 'SELECT muelles.iid, instalaciones.capacidad, permisos.atraque
+RETURN QUERY EXECUTE 'SELECT muelles.iid, instalaciones.capacidad
 		FROM muelles, para_m, permisos, instalaciones
 		WHERE instalaciones.iid = muelles.iid and muelles.iid = para_m.iid and para_m.per_id = permisos.per_id and permisos.atraque >= $1 and permisos.atraque <= $2 GROUP BY muelles.iid'
 	USING t1,t2;
