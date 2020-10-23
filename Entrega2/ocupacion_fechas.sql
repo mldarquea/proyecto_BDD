d18ca998f6545e2ca3371525f28943ebe1bec2a3
mldarquea <mldarquea@uc.cl>
2020-10-23 18:30:56 -0300

    o787

--- Entrega2/ocupacion_fechas.sql
+++ Entrega2/ocupacion_fechas.sql
@@ -8,8 +8,7 @@ DECLARE
 	mayor DATE;
 
 BEGIN
-	CREATE TEMP TABLE fecha_ocupada(iid int, capacidad int, fecha date, cantidad int);
-	CREATE TEMP TABLE fecha_ocupada2(iid int, capacidad int, fecha date, cantidad int);
+	CREATE TEMP TABLE fecha_ocupada(iid int, fecha date, cantidad int);
 
 	FOR tupla in SELECT * FROM (SELECT A.iid, A.capacidad, permisos.per_id,permisos.atraque,permisos_astilleros.salida
 FROM para_a,permisos_astilleros,permisos,(SELECT instalaciones.iid, instalaciones.capacidad
@@ -21,7 +20,7 @@ WHERE para_a.iid = A.iid and permisos_astilleros.per_id = para_a.per_id and perm
 	mayor := tupla.salida;
 	while menor <= mayor
 	LOOP
-	INSERT INTO fecha_ocupada VALUES(tupla.iid,tupla.capacidad, menor, 1);
+	INSERT INTO fecha_ocupada VALUES(tupla.iid, menor, 1);
 	menor = menor + 1;
 	END LOOP;
 	END LOOP;
@@ -32,11 +31,11 @@ FROM instalaciones, muelles WHERE instalaciones.iid = muelles.iid) as M
 WHERE para_m.iid = M.iid  and permisos.per_id = para_m.per_id) AS Inter
 					WHERE Inter.atraque  >= $1 and Inter.atraque <= $2 
 	LOOP
-	INSERT INTO fecha_ocupada2 VALUES(tupla2.iid,tupla2.capacidad,tupla2.atraque, 1);
+	INSERT INTO fecha_ocupada2 VALUES(tupla2.iid,tupla2.atraque, 1);
 	END LOOP;
 
 RETURN QUERY 
-SELECT * FROM fecha_ocupada2;
-DROP TABLE fecha_ocupada2;
+SELECT * FROM fecha_ocupada;
+DROP TABLE fecha_ocupada;
 END;
 $$ language plpgsql;
