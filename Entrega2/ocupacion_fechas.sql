79cb9f2f72e2155b242960e5687c9c4437206cc3
mldarquea <mldarquea@uc.cl>
2020-10-23 16:36:06 -0300

    1

--- Entrega2/ocupacion_fechas.sql
+++ Entrega2/ocupacion_fechas.sql
@@ -15,9 +15,7 @@ FROM instalaciones, astilleros WHERE instalaciones.iid = astilleros.iid) as A
 WHERE para_a.iid = A.iid and permisos_astilleros.per_id = para_a.per_id and permisos.per_id = permisos_astilleros.per_id) AS I
 					WHERE I.atraque  >= $1 and I.salida <= $2 
 	LOOP
-	menor := I.atraque;
-	mayor := I.salida;
-	INSERT INTO fecha_ocupada VALUES(tupla.iid,tupla.menor, 1);
+	INSERT INTO fecha_ocupada VALUES(tupla.iid,tupla.atraque, 1);
 	END LOOP;
 
 RETURN QUERY 
