fe2303bba8f927c76f40022808608f41cc6d1bf6
mldarquea <mldarquea@uc.cl>
2020-10-23 17:21:42 -0300

    12

--- Entrega2/ocupacion_fechas.sql
+++ Entrega2/ocupacion_fechas.sql
@@ -17,9 +17,9 @@ WHERE para_a.iid = A.iid and permisos_astilleros.per_id = para_a.per_id and perm
 	LOOP
 	DECLARE menor DATE := tupla.atraque;
 	DECLARE mayor DATE := tupla.salida;
-		WHILE menor <= mayor
 		LOOP
-		INSERT INTO fecha_ocupada VALUES(tupla.iid,tupla.atraque, 1);
+			if menor <= mayor
+			INSERT INTO fecha_ocupada VALUES(tupla.iid,tupla.atraque, 1);
 		menor = DATEADD(day, 1, menor);
 		END LOOP;
 	END LOOP;
