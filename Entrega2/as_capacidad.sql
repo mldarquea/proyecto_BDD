70562409eee4825b8d5da8778d8149a816cd27d3
mldarquea <mldarquea@uc.cl>
2020-10-24 14:45:50 -0300

    sinas

--- Entrega2/as_capacidad.sql
+++ Entrega2/as_capacidad.sql
@@ -28,9 +28,9 @@ WHERE para_a.iid = A.iid and permisos_astilleros.per_id = para_a.per_id and perm
 	END LOOP;
 	END LOOP;
 
-    FOR tupla_random in SELECT ((RAND()*(900)+1000) as random) 
+    FOR tupla_random in SELECT (RAND()*(900)+1000)  
     LOOP
-    per_id := tupla_random.random;
+    per_id := tupla_random;
     END LOOP;
     
 ------- Se muestran todas las instalaciones con capacidad y se registra un permiso para el primero disponible
